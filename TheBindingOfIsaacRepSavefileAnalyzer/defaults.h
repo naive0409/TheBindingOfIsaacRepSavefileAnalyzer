6c314fff8ed1d269a830724a4cd6dc01ac1c5a67
Wu Sanmu <iswurui@foxmail.com>
2023-09-14 12:53:29 +0800

    Update defaults.h

--- TheBindingOfIsaacRepSavefileAnalyzer/defaults.h
+++ TheBindingOfIsaacRepSavefileAnalyzer/defaults.h
@@ -2,5 +2,5 @@
 
 using namespace std;
 
-//const char DEFAULT_SAVEFILE_DIR[] = "C:\\Users\\Administrator\\Documents\\My Games\\Binding of Isaac Repentance\\20230912.rep_persistentgamedata1.dat";
-const char DEFAULT_SAVEFILE_DIR[] = "C:\\dev\\TheBindingOfIsaacRepSavefileAnalyzer\\ref\\20230912.rep_persistentgamedata1.dat";
+const char DEFAULT_SAVEFILE_DIR[] = "C:\\Users\\Administrator\\Documents\\My Games\\Binding of Isaac Repentance\\20230912.rep_persistentgamedata1.dat";
+//const char DEFAULT_SAVEFILE_DIR[] = "C:\\dev\\TheBindingOfIsaacRepSavefileAnalyzer\\ref\\20230912.rep_persistentgamedata1.dat";
